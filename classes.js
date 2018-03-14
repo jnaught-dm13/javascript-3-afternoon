5900b3cfa70ba9be8c189590c866e3cd4173cd41
Aaron.Nacua <alnacua@gmail.com>
2018-03-14 14:48:02 -0500

    classes.js Completed

--- classes.js
+++ classes.js
@@ -28,10 +28,18 @@
 
   Call your class Employee and receive all the data in the constructor in the order listed above.
 */
-
+class Employee {
+  constructor(first_name, last_name, email, age) {
+    this.first_name = first_name;
+    this.last_name = last_name;
+    this.email = email;
+    this.age = age;
+  }
+  makeWidget() {
+    return this.first_name + " " + this.last_name + " Widget";
+  }
+}
 //Code Here
-
-
 
 ////////// PROBLEM 2 //////////
 
@@ -48,10 +56,19 @@
 
   Call your new class Manager
 */
-
+class Manager extends Employee {
+  constructor(first_name, last_name, email, age, reports) {
+    super(first_name, last_name, email, age);
+    this.reports = [];
+  }
+  hire(newEmployee) {
+    this.reports.push(newEmployee);
+  }
+  fire(newEmployee) {
+    this.reports.splice(newEmployee, 1);
+  }
+}
 //Code Here
-
-
 
 ////////// PROBLEM 3 //////////
 
@@ -74,10 +91,38 @@
 
   Call your new class ProgressiveManager
 */
-
+class ProgressiveManager extends Manager {
+  constructor(firstName, lastName, email, age) {
+    super(firstName, lastName, email, age);
+    this.title = "Not a manager";
+    this.bonus = 0;
+  }
+  hire(employee) {
+    this.reports.push(employee);
+    this.updateTitle();
+  }
+  fire(index) {
+    this.reports.splice(index, 1);
+    this.bonus += 100;
+    this.updateTitle();
+  }
+  updateTitle() {
+    if (this.reports.length === 0) {
+      this.title = "Not a manager";
+    } else if (this.reports.length <= 3) {
+      this.title = "Barely Manager";
+    } else if (this.reports.length <= 10) {
+      this.title = "Mostly Manager";
+    } else if (this.reports.length <= 50) {
+      this.title = "Manager";
+    } else if (this.reports.length <= 100) {
+      this.title = "Manager Plus";
+    } else if (this.reports.length >= 101) {
+      this.title = "Bestest Manager";
+    }
+  }
+}
 //Code Here
-
-
 
 ////////// PROBLEM 4 - Black Diamond //////////
 
@@ -101,7 +146,24 @@
         - This function returns a function that is called when the machine is done rebooting
         - It should set decrease wear_and_tear_count by 10, and set needs_reboot to false
 */
-
+class Machine {
+  constructor() {
+    this.widgets_made_count = 0;
+    this.wear_and_tear_count = 0;
+    this.needs_reboot = false;
+  }
+  makeWidgets(num) {
+    this.widgets_made_count += num;
+    this.wear_and_tear_count += num / 50;
+  }
+  fixMachine() {
+    this.needs_reboot = true;
+  }
+  reboot() {
+    return () => {
+      this.wear_and_tear_count -= 10;
+      this.needs_reboot = false;
+    };
+  }
+}
 //Code Here
-
-
